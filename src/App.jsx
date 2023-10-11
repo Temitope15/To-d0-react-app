ec9ca4d9a76022d90afcbe537f83dc6ccde59543
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-10-11 16:06:59 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl underline text-red-500">Hello World!</h1>
+      <h1 className="text-2xl underline text-red-500">Hello World!</h1>
     </>
   )
 }
